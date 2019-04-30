6f7b7212f6880cdf3e04b2f672bc8750d3d0a1a6
Mudzakkir <707089+qaharmdz@users.noreply.github.com>
2019-04-30 18:39:31 +0700

    Update license notice

--- mocha/admin/Component/Account/Controller/User.php
+++ mocha/admin/Component/Account/Controller/User.php
@@ -1,12 +1,12 @@
 <?php
-/*
- * This file is part of Mocha package.
+/**
+ * This file is part of Mocha.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
  * Released under GPL version 3 or any later version.
- * Full copyright and license see LICENSE file or visit https://www.gnu.org/licenses/gpl-3.0.en.html.
+ * Copyright and license see LICENSE file or https://www.gnu.org/licenses/gpl-3.0.en.html.
  */
 
 namespace Mocha\Admin\Component\Account\Controller;
